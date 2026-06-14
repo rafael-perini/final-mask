3827145ffeac4e8eaeb09e5e2fb99a8825ca7482
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-14 21:33:41 +0000

    chore: initial commit

---
+++ src/__tests__/formatter.test.ts
@@ -0,0 +1,50 @@
+import { expect, describe, it } from "vite-plus/test";
+import Formatter from "../formatter.ts";
+
+describe("formatter", () => {
+  const mask = "+## (##) ####-####";
+  const formatter = new Formatter(mask);
+
+  describe("mask", () => {
+    it("should format a string according to the masked value", () => {
+      const maskedPhone = "+55 (11) 1224-1426";
+      const unmaskedPhone = "551112241426";
+
+      const value = formatter.mask(unmaskedPhone);
+      expect(value).toBe(maskedPhone);
+    });
+
+    it("should return empty if there's no value yet", () => {
+      const value = formatter.mask("");
+      expect(value).toBe("");
+    });
+
+    it(" should be abble to format already formated values", () => {
+      const maskedPhone = "+55 (11) 1224-1426";
+      const value = formatter.mask(maskedPhone);
+      expect(value).toBe(maskedPhone);
+
+      const partiallyMaskedPhone = "+55 (11) 1224-1426";
+      const value = formatter.mask(partialllyMaskedPhone);
+      expect(value).toBe(maskedPhone);
+    });
+
+    it.todo(" should be abble to format partial values", () => {
+      const maskedPhone = "+55 (11) 1224";
+      const unmaskedPhone = "55111224";
+
+      const partialValue = formatter.mask(unmaskedPhone);
+      expect(partialValue).toBe(maskedPhone);
+    });
+  });
+
+  describe("unmask", () => {
+    it("should return a string composed only by numbers from a mask", () => {
+      const maskedPhone = "+55 (11) 1224-1426";
+      const unmaskedPhone = "551112241426";
+
+      const value = formatter.unmask(maskedPhone);
+      expect(value).toBe(unmaskedPhone);
+    });
+  });
+});
---
+++ src/formatter.ts
@@ -0,0 +1,26 @@
+export default class Formatter {
+  private _mask: string;
+
+  /**
+   * @param mask A masked string like "+ ## (##) #####-####"
+   */
+  constructor(mask: string) {
+    this._mask = mask;
+  }
+
+  mask(value: string) {
+    const unmaskedValue = this.unmask(value);
+    if (!unmaskedValue) return "";
+
+    let maskedValue = this._mask;
+    for (const number of unmaskedValue) {
+      maskedValue = maskedValue.replace(/#/, number);
+    }
+
+    return maskedValue;
+  }
+
+  unmask(value: string) {
+    return value.replace(/[\D]/g, "");
+  }
+}
