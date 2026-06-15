c3016e11dc68c9f4a6fe88a430296f9c17f88583
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-15 01:24:42 +0000

    feat(mask): add mask for input

---
+++ src/utils/input/mask.ts
@@ -0,0 +1,56 @@
+import Formatter from "@/utils/format/formatter.ts";
+
+export default class Mask {
+  private _formatter: Formatter;
+
+  constructor(...args: ConstructorParameters<typeof Formatter>) {
+    this._formatter = new Formatter(...args);
+  }
+
+  init(selectorOrElement: string | HTMLInputElement) {
+    const input = this.toHTMLInputElement(selectorOrElement);
+    if (!input) {
+      return console.warn(
+        `[final-mask]: Failed to find the associated input element for: ${selectorOrElement as string}`,
+      );
+    }
+
+    if (!this.isHTMLInputElement(input)) {
+      return console.warn(
+        `[final-mask]: Associated input element for "${this.toString(selectorOrElement)}" is not an input`,
+      );
+    }
+
+    input.oninput = (event) => this.handleInput(event);
+  }
+
+  private handleInput(event: InputEvent) {
+    const element = event.target;
+    if (!this.isHTMLInputElement(element)) return;
+
+    const { value } = element;
+    const maskedValue = this._formatter.mask(value);
+    element.setRangeText(maskedValue, 0, value.length, "end");
+  }
+
+  private toString(selectorOrElement: string | HTMLElement) {
+    if (this.isSelector(selectorOrElement)) return selectorOrElement;
+    return selectorOrElement.outerHTML;
+  }
+
+  private toHTMLInputElement(
+    selectorOrElement: string | HTMLInputElement,
+  ): HTMLInputElement | null {
+    return this.isSelector(selectorOrElement)
+      ? document.querySelector(selectorOrElement)
+      : selectorOrElement;
+  }
+
+  private isSelector(selectorOrElement: string | Element): selectorOrElement is string {
+    return typeof selectorOrElement === "string";
+  }
+
+  private isHTMLInputElement(element: Element | EventTarget | null): element is HTMLInputElement {
+    return !!(element instanceof HTMLInputElement);
+  }
+}
