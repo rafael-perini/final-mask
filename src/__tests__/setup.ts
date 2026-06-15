bd8a2be78dc922f2f23bcf5dce64774b6afff94b
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-15 00:26:31 +0000

    test: add listeners for console

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,5 +1,7 @@
 import { beforeAll, vi } from "vite-plus/test";
 
 beforeAll(() => {
+  vi.spyOn(console, "error").mockImplementation(() => {});
+  vi.spyOn(console, "warn").mockImplementation(() => {});
   vi.useFakeTimers();
 });
