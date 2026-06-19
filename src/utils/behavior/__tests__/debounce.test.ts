060e1469205b0e1090a35474e1a9dc2d42d0a704
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-19 23:03:31 +0000

    test(debounce): validate flush method

--- src/utils/behavior/__tests__/debounce.test.ts
+++ src/utils/behavior/__tests__/debounce.test.ts
@@ -11,8 +11,34 @@ describe("Debounce", () => {
       expect(fakeFunction).not.toHaveBeenCalled();
 
       vi.advanceTimersByTime(125);
-      expect(fakeFunction).toHaveBeenCalled();
+      expect(fakeFunction).toHaveBeenCalledOnce();
       expect(fakeFunction).toHaveBeenCalledWith(...fakeParams);
+    });
+
+    it("should run only once after timer", () => {
+      proxyFunction.run();
+      proxyFunction.run();
+
+      vi.runAllTimers();
+      expect(fakeFunction).toHaveBeenCalledOnce();
+    });
+  });
+
+  describe("flush", () => {
+    it("should run the pending callbacks immediately", () => {
+      const expectedArgs = [4, 5, 6];
+      proxyFunction.run();
+      proxyFunction.run(expectedArgs);
+
+      proxyFunction.flush();
+      expect(fakeFunction).toHaveBeenCalledOnce();
+      expect(fakeFunction).toHaveBeenCalledWith(expectedArgs);
+
+      vi.runAllTimers();
+      expect(fakeFunction).toHaveBeenCalledOnce();
+
+      proxyFunction.flush();
+      expect(fakeFunction).toHaveBeenCalledOnce();
     });
   });
 });
