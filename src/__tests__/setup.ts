5e5b3973d88a8a482e8511fe5573dc38aba3b122
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-20 00:01:38 +0000

    chore: remove unneeded fake timers from test setup

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -3,5 +3,4 @@ import { beforeAll, vi } from "vite-plus/test";
 beforeAll(() => {
   vi.spyOn(console, "error").mockImplementation(() => {});
   vi.spyOn(console, "warn").mockImplementation(() => {});
-  vi.useFakeTimers();
 });
