6d0d968f09851273dbace9c757ab027eb8c2e372
Rafael Perini <perini.de.souza.rafael@gmail.com>
2026-06-21 15:35:29 -0300

    Revert "Add CD (#5)" (#6)
    This reverts commit c38f1419cea8b9a17c67aad9e8b9ddee9aebd0b2.

--- src/__tests__/setup.ts
+++
@@ -1,6 +0,0 @@
-import { beforeAll, vi } from "vite-plus/test";
-
-beforeAll(() => {
-  vi.spyOn(console, "error").mockImplementation(() => {});
-  vi.spyOn(console, "warn").mockImplementation(() => {});
-});
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as FinalMask } from "@/utils/input/mask.ts";
