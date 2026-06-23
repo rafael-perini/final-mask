08cfb4daaf1ab315397b3cfbe67832305e4be44a
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-23 02:27:20 +0000

    test(mask): validate maxlength property and improve typing validations

--- src/utils/input/__tests__/mask.test.ts
+++ src/utils/input/__tests__/mask.test.ts
@@ -38,6 +38,11 @@ describe("Mask", () => {
         `[final-mask]: Associated input element for "${h1?.outerHTML}" is not an input`,
       );
     });
+
+    it("should add a maxlength to the input", () => {
+      const mask = getMask();
+      expect(input.maxLength).toBe(mask.length);
+    });
   });
 
   describe.todo("getMaskedValue");
@@ -75,37 +80,40 @@ describe("Mask", () => {
   describe("undo and redo", () => {
     it("should handle ordinary typing", () => {
       const firstValue = "12";
-      const maskedFirstValue = "12";
 
       setValue(input, firstValue);
+
+      const secondValue = "123";
+      const maskedSecondValue = "12.3";
+      setValue(input, secondValue);
 
       undo(input);
       expect(input.value).toBe("");
 
       redo(input);
-      expect(input.value).toBe(maskedFirstValue);
-
-      const secondValue = "123456789";
-      const maskedSecondValue = "12.345.678/9";
-
-      setValue(input, secondValue);
-
-      undo(input);
-      expect(input.value).toBe(maskedFirstValue);
-
-      redo(input);
       expect(input.value).toBe(maskedSecondValue);
 
-      const thirdValue = "1235468793210";
-      const maskedThirdValue = "12.354.687/9321-0";
+      const thirdValue = "123456789";
+      const maskedThirdValue = "12.345.678/9";
 
       setValue(input, thirdValue);
 
       undo(input);
       expect(input.value).toBe(maskedSecondValue);
 
+      redo(input);
+      expect(input.value).toBe(maskedThirdValue);
+
+      const fourthValue = "1235468793210";
+      const maskedFourthValue = "12.354.687/9321-0";
+
+      setValue(input, fourthValue);
+
       undo(input);
-      expect(input.value).toBe(maskedFirstValue);
+      expect(input.value).toBe(maskedThirdValue);
+
+      undo(input);
+      expect(input.value).toBe(maskedSecondValue);
 
       undo(input);
       expect(input.value).toBe("");
@@ -113,7 +121,7 @@ describe("Mask", () => {
       redo(input);
       redo(input);
       redo(input);
-      expect(input.value).toBe(maskedThirdValue);
+      expect(input.value).toBe(maskedFourthValue);
 
       undo(input);
       undo(input);
@@ -122,14 +130,13 @@ describe("Mask", () => {
     });
 
     it("should handle text deletions", () => {
-      const firstValue = "43210987654321";
-      const maskedFirstValue = "43.210.987/6543-21";
+      const firstValue = "43.210.987/6543-21";
 
       setValue(input, firstValue);
       deleteBackward(input);
 
       undo(input);
-      expect(input.value).toBe(maskedFirstValue);
+      expect(input.value).toBe(firstValue);
 
       redo(input);
       expect(input.value).toBe("43.210.987/6543-2");
@@ -167,20 +174,19 @@ describe("Mask", () => {
     });
 
     it("should handle blur", () => {
-      const firstValue = "78945612309876";
+      const firstValue = "78.945.612/3098-76";
 
       setValue(input, firstValue);
       blurLeavingPage(input);
 
-      const secondValue = "87678945612309";
-      const maskedSecondValue = "87.678.945/6123-09";
+      const secondValue = "87.678.945/6123-09";
 
       setValue(input, secondValue);
       undo(input);
       expect(input.value).toBe("");
 
       redo(input);
-      expect(input.value).toBe(maskedSecondValue);
+      expect(input.value).toBe(secondValue);
 
       const thirdValue = "86789456123";
       const maskedThirdValue = "86.789.456/123";
@@ -245,9 +251,13 @@ function setupInput(options: SetupOptions = {}) {
   return input;
 }
 
-const mask = "##.###.###/####-##";
 function setupMask(input: string | HTMLInputElement, options?: SetupOptions) {
+  const mask = getMask();
   Mask.setup(mask, input, options);
+}
+
+function getMask() {
+  return "##.###.###/####-##";
 }
 
 function appendH1() {
