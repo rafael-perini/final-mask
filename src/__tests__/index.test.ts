2499ef31890d85ee17ea66af48da2d0951b801c1
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-23 21:26:53 +0000

    test(formatter): update index validations to check on formatter

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,12 @@
+import Formatter from "@/utils/format/formatter.ts";
 import Mask from "@/utils/input/mask.ts";
-import { FinalMask } from "@/index.ts";
+import { FinalMask, MaskFormatter } from "@/index.ts";
 
 describe("index", () => {
   it("should export mask as FinalMask", () => {
     expect(FinalMask).toBe(Mask);
+  });
+  it("should export formatter as MaskFormatter", () => {
+    expect(MaskFormatter).toBe(Formatter);
   });
 });
