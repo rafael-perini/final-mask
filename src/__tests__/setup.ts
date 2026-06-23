9c33acaf8779bfb63daa15858f2c669adef4f9b2
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-06-23 00:03:35 +0000

    test: mock console logs

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -3,4 +3,5 @@ import { beforeAll, vi } from "vite-plus/test";
 beforeAll(() => {
   vi.spyOn(console, "error").mockImplementation(() => {});
   vi.spyOn(console, "warn").mockImplementation(() => {});
+  vi.spyOn(console, "log").mockImplementation(() => {});
 });
